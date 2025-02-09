8d257fc7f77685f7c38f819d4f7b734fd7d2f550
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-09 13:17:05 +0100

    1341-SPLIT-A-STRING-IN-BALANCED-STRINGS Stats: Time: 0 ms (100%), Space: 38.6 MB (27.27%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cs
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public int BalancedStringSplit(string s) {
         ushort result = 0;
-        byte lCount = 0;
-        byte rCount = 0;
+        ushort lCount = 0;
+        ushort rCount = 0;
         for(ushort i = 0; i < s.Length; i++)
         {
             if(s[i] == 'R')
@@ -13,7 +13,7 @@ public class Solution {
             {
                 lCount++;
             }
-            if(rCount == lCount && rCount > 0)
+            if(rCount == lCount)
             {
                 result++;
                 lCount = 0;
