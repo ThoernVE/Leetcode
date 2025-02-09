47e0fe7fe176c81d620156030d0da2163b0e5026
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-09 13:12:21 +0100

    1341-SPLIT-A-STRING-IN-BALANCED-STRINGS Stats: Time: 0 ms (100%), Space: 38.6 MB (27.27%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cs
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cs
@@ -1,9 +1,9 @@
 public class Solution {
     public int BalancedStringSplit(string s) {
         ushort result = 0;
-        ushort lCount = 0;
-        ushort rCount = 0;
-        for(int i = 0; i < s.Length; i++)
+        byte lCount = 0;
+        byte rCount = 0;
+        for(ushort i = 0; i < s.Length; i++)
         {
             if(s[i] == 'R')
             {
