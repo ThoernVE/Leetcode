be96c0290a43a815c1be52ca4d9a2d45981718f0
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-09 13:32:11 +0100

    2881-SPLIT-STRINGS-BY-SEPARATOR Stats: Time: 2 ms (100%), Space: 63 MB (51.43%) - LeetHub

--- 2881-split-strings-by-separator/2881-split-strings-by-separator.cs
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.cs
@@ -1,16 +1,5 @@
 public class Solution {
     public IList<string> SplitWordsBySeparator(IList<string> words, char separator) {
-        List<string> result = new List<string>();
-
-        foreach(string word in words)
-        {
-            string[] split = word.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-            foreach(string s in split)
-            {
-                result.Add(s);
-            }
-        }
-
-        return result;
+        return String.Join(separator, words).Split(separator, StringSplitOptions.RemoveEmptyEntries);
     }
 }
