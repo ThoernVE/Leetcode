50be13f84915d2602a7b3b6fdeac658262fca108
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-13 21:48:15 +0100

    3227-FIND-MISSING-AND-REPEATED-VALUES Stats: Time: 15 ms (40%), Space: 56.4 MB (23.53%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cs
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cs
@@ -2,22 +2,20 @@ public class Solution {
     public int[] FindMissingAndRepeatedValues(int[][] grid) {
         int[] ans = new int[2];
         int n = grid.Length;
-        int m = grid[0].Length;
-        int rep = -1;
 
-        int expectedSum = ((n*m)*((n*m)+1))/2;
-        int max = grid.GetLength(0) * grid.GetLength(0);
+        int expectedSum = ((n*n)*((n*n)+1))/2;
         HashSet<int> set = [];
 
-
+        bool japp = false;
         foreach(int[] arr in grid)
         {
             foreach(int num in arr)
             {
                 
-                if(set.Contains(num))
+                if(!japp && set.Contains(num))
                 {
                     ans[0] = num;
+                    japp = true;
                 }
                 else
                 {
