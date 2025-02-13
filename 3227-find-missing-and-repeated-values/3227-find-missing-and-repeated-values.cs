84c914e1722e347f1c5013ede7d888dbd57869be
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-13 21:46:08 +0100

    3227-FIND-MISSING-AND-REPEATED-VALUES Stats: Time: 16 ms (31.76%), Space: 56.5 MB (18.82%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cs
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cs
@@ -1,40 +1,33 @@
 public class Solution {
     public int[] FindMissingAndRepeatedValues(int[][] grid) {
         int[] ans = new int[2];
+        int n = grid.Length;
+        int m = grid[0].Length;
+        int rep = -1;
+
+        int expectedSum = ((n*m)*((n*m)+1))/2;
         int max = grid.GetLength(0) * grid.GetLength(0);
-        int[] loop = new int[max];
-        int count = 0;
+        HashSet<int> set = [];
+
 
         foreach(int[] arr in grid)
         {
             foreach(int num in arr)
             {
-                loop[count++] = num;
+                
+                if(set.Contains(num))
+                {
+                    ans[0] = num;
+                }
+                else
+                {
+                    set.Add(num);
+                    expectedSum -= num;
+                }
             }
         }
+        ans[1] = expectedSum;
 
-        Array.Sort(loop);
-        if(loop[0] == 2)
-        {
-            ans[1] = 1;
-        }
-
-
-        for(int i = 0; i < loop.Length -1; i++)
-        {
-            if(loop[i] == loop[i + 1])
-            {
-                ans[0] = loop[i];
-            }
-            else if(loop[i + 1] > loop[i] + 1)
-            {
-                ans[1] = loop[i] + 1;
-            }
-        }
-        if(ans[1] == 0)
-        {
-            ans[1] = loop.Length;
-        }
         return ans;
     }
 }
