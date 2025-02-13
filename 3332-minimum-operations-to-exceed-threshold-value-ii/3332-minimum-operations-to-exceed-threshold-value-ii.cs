b318e0078bd82b73ca4d9b1220d85ed8f6dd320c
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-13 11:28:14 +0100

    3332-MINIMUM-OPERATIONS-TO-EXCEED-THRESHOLD-VALUE-II Stats: Time: 364 ms (94.12%), Space: 79.2 MB (94.12%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cs
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cs
@@ -1,103 +1,28 @@
 public class Solution {
-    public int MinOperations(int[] nums, int k)
-    {
-        int count = 0;
-        var minHeap = new PriorityQueue<long>();
+    public int MinOperations(int[] nums, int k) {
+        var pq = new PriorityQueue<int, int>();
 
-        // Add all elements to the min-heap
-        foreach (var num in nums)
+        foreach(var num in nums)
         {
-            minHeap.Enqueue(num);
+            if(num < k)
+            {
+                pq.Enqueue(num, num);
+            }
         }
 
-        // Continue until the smallest element is >= k
-        while (minHeap.Count > 0 && minHeap.Peek() < k)
+        int count = 0;
+        while(pq.Count > 1)
         {
-            if (minHeap.Count < 2)
+            var x = pq.Dequeue();
+            var y = pq.Dequeue();
+
+            if(x * 2  < k - y)
             {
-                // If there's only one element left and it's < k, we can't proceed
-                return -1; // Or handle this case as per your requirements
+                pq.Enqueue(x * 2 + y, x * 2 + y);
             }
-
-            // Perform the operation
-            long smallest = minHeap.Dequeue();
-            long secondSmallest = minHeap.Dequeue();
-            long newElement = smallest * 2 + secondSmallest;
-
-            minHeap.Enqueue(newElement);
             count++;
         }
+        return count + pq.Count;
 
-        return count;
-    }
-}
-
-// PriorityQueue implementation (min-heap)
-public class PriorityQueue<T> where T : IComparable<T>
-{
-    private List<T> heap;
-
-    public PriorityQueue()
-    {
-        heap = new List<T>();
-    }
-
-    public int Count => heap.Count;
-
-    public void Enqueue(T item)
-    {
-        heap.Add(item);
-        int i = heap.Count - 1;
-        while (i > 0)
-        {
-            int parent = (i - 1) / 2;
-            if (heap[parent].CompareTo(heap[i]) <= 0)
-                break;
-            Swap(parent, i);
-            i = parent;
-        }
-    }
-
-    public T Dequeue()
-    {
-        if (heap.Count == 0)
-            throw new InvalidOperationException("Queue is empty");
-
-        T result = heap[0];
-        int last = heap.Count - 1;
-        heap[0] = heap[last];
-        heap.RemoveAt(last);
-
-        int i = 0;
-        while (true)
-        {
-            int left = 2 * i + 1;
-            int right = 2 * i + 2;
-            if (left >= heap.Count)
-                break;
-            int minChild = left;
-            if (right < heap.Count && heap[right].CompareTo(heap[left]) < 0)
-                minChild = right;
-            if (heap[i].CompareTo(heap[minChild]) <= 0)
-                break;
-            Swap(i, minChild);
-            i = minChild;
-        }
-
-        return result;
-    }
-
-    public T Peek()
-    {
-        if (heap.Count == 0)
-            throw new InvalidOperationException("Queue is empty");
-        return heap[0];
-    }
-
-    private void Swap(int i, int j)
-    {
-        T temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
     }
 }
