efc5d1d383a69cfd14fa7f9ebe92e8fb7d2cd790
ThoernVE <viktor.e.thorn@hotmail.com>
2025-02-18 11:22:46 +0100

    2456-CONSTRUCT-SMALLEST-NUMBER-FROM-DI-STRING Stats: Time: 3 ms (42.86%), Space: 40.6 MB (21.43%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cs
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cs
@@ -6,25 +6,11 @@ public class Solution {
         for(int i = 0; i < pattern.Length + 1; i++)
         {
             stk.Push(i + 1);
-            if(i >= pattern.Length)
-            {
-                continue;
-            }
-            else if(pattern[i] == 'I')
-            {
-                while(stk.Count > 0)
-                {
-                    list.Add((int)stk.Pop());
-                }
-            }
-        }
-
-        while(stk.Count > 0)
+            while(stk.Count > 0 && (i >= pattern.Length || pattern[i] == 'I'))
             {
                 list.Add((int)stk.Pop());
-            }
-
-
+            }   
+        }
 
         return string.Join("", list);
     }
